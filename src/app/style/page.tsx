6ce90ebbd1b84ebe06fa8b852052a6e9d8654e0d
Aron Weston <aron@aronweston.com>
2024-02-08 22:34:18 +1100

    feat: init

---
+++ src/app/style/page.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+
+export default async function Styles() {
+  return (
+    <div className="grid grid-cols-4 container gap-5 ">
+      <div className="bg-background text-foreground p-8">
+        <h3>Background</h3>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus
+          explicabo sed architecto. Voluptatem veniam quis eligendi vitae aspernatur cum non labore
+          quod veritatis tempora harum, ipsa eaque, laboriosam distinctio?
+        </p>
+      </div>
+      <div className="bg-primary text-primary-foreground p-8">
+        <h3>Primary</h3>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus
+          explicabo sed architecto. Voluptatem veniam quis eligendi vitae aspernatur cum non labore
+          quod veritatis tempora harum, ipsa eaque, laboriosam distinctio?
+        </p>
+      </div>
+
+      <div className="bg-secondary text-secondary-foreground p-8">
+        <h3>Secondary</h3>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus
+          explicabo sed architecto. Voluptatem veniam quis eligendi vitae aspernatur cum non labore
+          quod veritatis tempora harum, ipsa eaque, laboriosam distinctio?
+        </p>
+      </div>
+      <div className="bg-muted text-muted-foreground p-8">
+        <h3>Muted</h3>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus
+          explicabo sed architecto. Voluptatem veniam quis eligendi vitae aspernatur cum non labore
+          quod veritatis tempora harum, ipsa eaque, laboriosam distinctio?
+        </p>
+      </div>
+
+      <div className="bg-accent text-accent-foreground p-8">
+        <h3>Accent</h3>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus
+          explicabo sed architecto. Voluptatem veniam quis eligendi vitae aspernatur cum non labore
+          quod veritatis tempora harum, ipsa eaque, laboriosam distinctio?
+        </p>
+      </div>
+
+      <div className="bg-card text-card-foreground p-8">
+        <h3>Card</h3>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus
+          explicabo sed architecto. Voluptatem veniam quis eligendi vitae aspernatur cum non labore
+          quod veritatis tempora harum, ipsa eaque, laboriosam distinctio?
+        </p>
+      </div>
+
+      <div className="bg-popover text-popover-foreground p-8">
+        <h3>Popover</h3>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus
+          explicabo sed architecto. Voluptatem veniam quis eligendi vitae aspernatur cum non labore
+          quod veritatis tempora harum, ipsa eaque, laboriosam distinctio?
+        </p>
+      </div>
+
+      <div className="bg-destructive text-destructive-foreground p-8">
+        <h3>Destructive</h3>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus
+          explicabo sed architecto. Voluptatem veniam quis eligendi vitae aspernatur cum non labore
+          quod veritatis tempora harum, ipsa eaque, laboriosam distinctio?
+        </p>
+      </div>
+
+      <div className="bg-success text-success-foreground p-8">
+        <h3>Success</h3>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus
+          explicabo sed architecto. Voluptatem veniam quis eligendi vitae aspernatur cum non labore
+          quod veritatis tempora harum, ipsa eaque, laboriosam distinctio?
+        </p>
+      </div>
+    </div>
+  );
+}
